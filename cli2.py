0b1d051a0111c11ad408545c7553570e2fecfb54
hudbuz <hzb2@cornell.edu>
2017-01-16 08:07:23 -0500

    organzied as class and class method call

--- cli2.py
+++ cli2.py
@@ -14,33 +14,32 @@ output = ''
 
 def get_arguments(data_path, number_of_items, output_path, arguments):
     params_args = {}
-    for i in range(0,len(arguments)):
-        if arguments[i] == '-d':
-             params_args['dir_path'] = os.path.abspath(arguments[i+1])
-        elif arguments[i] == '-n':
-             params_args['query_num'] = arguments[i+1]
-        elif arguments[i] == '-o':
-            
-             params_args['output'] = arguments[i+1]
+    for i in range(0,len(args)):
+        if args[i] == '-d':
+             params_args['dir_path'] = os.path.abspath(args[i+1])
+        elif args[i] == '-n':
+             params_args['query_num'] = args[i+1]
+        elif args[i] == '-o':
+             params_args['output'] = args[i+1]
              ####make sure this is absolute
     return params_args
 
 params = get_arguments(dir_path, query_num, output, args)
 
 
+out_params = {'source_folder': params['dir_path'], 'run_date': str(dt.date(2017, 1, 14)),'file_count': len(data), 'best_sellers': [] }
 collumn_names = ['product_id', 'product_name', 'qty_sold']
 
-class File_Reader:
+class = File_Reader:
 
 
-    def __init__(self, path_to_dir, number_of_products, output_filepath, collumns):
+    def __init__(self, path_to_dir, number_of_products, output_filepath ):
         self.directory_path = path_to_dir
         self.product_numb = number_of_products
-        self.output_directory = output_filepath
-        self.col_names = collumns
+        self.outut_directory = output_filepath
 
 
-    def top_n_products(self):
+    def top_n_products(self, col_names):
 
         data = os.listdir(self.directory_path)
         d = []
@@ -58,12 +57,10 @@ class File_Reader:
 
                     d.append({'product_id': products[p]['product_id'], 'product_name': products[p]['product_name'], 'qty_sold': products[p]['qty_sold']})
 
-        df = pds.DataFrame(data=d, columns=self.col_names)
-
+        df = pds.DataFrame(data=d, columns=col_names)
         os.system('clear')
-
-        grouped = df.groupby('product_id').sum()
-        ordered = grouped.sort_values(by='qty_sold', ascending=False)
+        grouped = df.groupby(col_names[0], col_names[1]).sum()
+        ordered = grouped.sort_values(by=col_names[2], ascending=False)
 
         result = ordered.head(int(self.product_numb))
         # print(cache)
@@ -78,11 +75,11 @@ class File_Reader:
         filepath = os.path.join(params['output'], '/item-count-test.txt')
 
         file = open('item-count-test.txt', 'w')
-        file.write(json_result)
+        file.write(json_results)
         file.close()
-        newpath = self.output_directory + '/item-count-test.txt'
-        os.rename('item-count-test.txt', newpath)
+        newpath = params['output'] + '/item-count-test.txt'
+        os.rename('item-count-test.txt', self.output_directory+'/item-count-test.txt')
 
-query = File_Reader(params['dir_path'], params['query_num'], params['output'], collumn_names)
-query_result = query.top_n_products()
+query = File_Reader(params['dir_path'], params['query_num'], params['output'])
+query_result = query.top_n_products(collumn_names)
 new_file = query.create_and_send_file(query_result)
